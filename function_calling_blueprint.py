cf96dd3c6cf6fe997dc915c11c013d44a21e45e2
musicheardworldwide <158991319+musicheardworldwide@users.noreply.github.com>
2024-06-12 06:57:33 -0700

    Update function_calling_blueprint.py

--- function_calling_blueprint.py
+++ function_calling_blueprint.py
@@ -4,109 +4,75 @@ from schemas import OpenAIChatMessage
 import os
 import requests
 import json
-
-from utils.pipelines.main import (
-    get_last_user_message,
-    add_or_update_system_message,
-    get_tools_specs,
-)
-
+from utils.pipelines.main import get_last_user_message, add_or_update_system_message, get_tools_specs
 
 class Pipeline:
     class Valves(BaseModel):
-        # List target pipeline ids (models) that this filter will be connected to.
-        # If you want to connect this filter to all pipelines, you can set pipelines to ["*"]
         pipelines: List[str] = []
-
-        # Assign a priority level to the filter pipeline.
-        # The priority level determines the order in which the filter pipelines are executed.
-        # The lower the number, the higher the priority.
         priority: int = 0
-
-        # Valves for function calling
-        OPENAI_API_BASE_URL: str
-        OPENAI_API_KEY: str
-        TASK_MODEL: str
-        TEMPLATE: str
+        openai_api_base_url: str
+        openai_api_key: str
+        task_model: str
+        template: str
 
     def __init__(self):
-        # Pipeline filters are only compatible with Open WebUI
-        # You can think of filter pipeline as a middleware that can be used to edit the form data before it is sent to the OpenAI API.
         self.type = "filter"
-
-        # Optionally, you can set the id and name of the pipeline.
-        # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
-        # The identifier must be unique across all pipelines.
-        # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
-        # self.id = "function_calling_blueprint"
         self.name = "Function Calling Blueprint"
-
-        # Initialize valves
         self.valves = self.Valves(
-            **{
-                "pipelines": ["*"],  # Connect to all pipelines
-                "OPENAI_API_BASE_URL": os.getenv(
-                    "OPENAI_API_BASE_URL", "https://api.openai.com/v1"
-                ),
-                "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY", "YOUR_OPENAI_API_KEY"),
-                "TASK_MODEL": os.getenv("TASK_MODEL", "gpt-3.5-turbo"),
-                "TEMPLATE": """Use the following context as your learned knowledge, inside <context></context> XML tags.
+            pipelines=["*"],
+            openai_api_base_url=os.getenv("OPENAI_API_BASE_URL", "https://api.groq.com/openai/v1"),
+            openai_api_key=os.getenv("OPENAI_API_KEY", "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"),
+            task_model=os.getenv("TASK_MODEL", "llama3-8b-8192"),
+            template='''
 <context>
-    {{CONTEXT}}
+  {{CONTEXT}}
 </context>
 
-When answer to user:
-- If you don't know, just say that you don't know.
-- If you don't know when you are not sure, ask for clarification.
-Avoid mentioning that you obtained the information from the context.
-And answer according to the language of the user's question.""",
-            }
+Task: Implement a function that takes two integers as input and returns their sum.
+
+Input: Two integers
+Output: A single integer
+
+Example inputs and expected outputs:
+  - Input: 2, 3, Expected output: 5
+  - Input: 5, 7, Expected output: 12
+
+Constraints:
+  - The function should not use recursion
+  - The function should be efficient and not exceed a time complexity of O(n)
+</context>
+'''
         )
 
     async def on_startup(self):
-        # This function is called when the server is started.
         print(f"on_startup:{__name__}")
-        pass
 
     async def on_shutdown(self):
-        # This function is called when the server is stopped.
         print(f"on_shutdown:{__name__}")
-        pass
 
     async def inlet(self, body: dict, user: Optional[dict] = None) -> dict:
-        # If title generation is requested, skip the function calling filter
         if body.get("title", False):
             return body
 
         print(f"pipe:{__name__}")
         print(user)
 
-        # Get the last user message
         user_message = get_last_user_message(body["messages"])
-
-        # Get the tools specs
         tools_specs = get_tools_specs(self.tools)
 
-        # System prompt for function calling
         fc_system_prompt = (
             f"Tools: {json.dumps(tools_specs, indent=2)}"
             + """
-If a function tool doesn't match the query, return an empty string. Else, pick a function tool, fill in the parameters from the function tool's schema, and return it in the format { "name": \"functionName\", "parameters": { "key": "value" } }. Only pick a function if the user asks.  Only return the object. Do not return any other text."
-"""
+If a function tool doesn't match the query, return an empty string. Else, pick a function tool, fill in the parameters from the function tool's schema, and return it in the format { "name": "functionName", "parameters": { "key": "value" } }. Only pick a function if the user asks.  Only return the object. Do not return any other text."""
         )
 
-        r = None
         try:
-            # Call the OpenAI API to get the function response
-            r = requests.post(
-                url=f"{self.valves.OPENAI_API_BASE_URL}/chat/completions",
+            async with requests.post(
+                url=f"{self.valves.openai_api_base_url}/chat/completions",
                 json={
-                    "model": self.valves.TASK_MODEL,
+                    "model": self.valves.task_model,
                     "messages": [
-                        {
-                            "role": "system",
-                            "content": fc_system_prompt,
-                        },
+                        {"role": "system", "content": fc_system_prompt},
                         {
                             "role": "user",
                             "content": "History:\n"
@@ -119,26 +85,21 @@ If a function tool doesn't match the query, return an empty string. Else, pick a
                             + f"Query: {user_message}",
                         },
                     ],
-                    # TODO: dynamically add response_format?
-                    # "response_format": {"type": "json_object"},
                 },
                 headers={
-                    "Authorization": f"Bearer {self.valves.OPENAI_API_KEY}",
+                    "Authorization": f"Bearer {self.valves.openai_api_key}",
                     "Content-Type": "application/json",
                 },
                 stream=False,
-            )
-            r.raise_for_status()
+            ) as response:
+                response.raise_for_status()
+                response_json = response.json()
+                content = response_json["choices"][0]["message"]["content"]
 
-            response = r.json()
-            content = response["choices"][0]["message"]["content"]
-
-            # Parse the function response
-            if content != "":
+            if content:
                 result = json.loads(content)
                 print(result)
 
-                # Call the function
                 if "name" in result:
                     function = getattr(self.tools, result["name"])
                     function_result = None
@@ -147,26 +108,20 @@ If a function tool doesn't match the query, return an empty string. Else, pick a
                     except Exception as e:
                         print(e)
 
-                    # Add the function result to the system prompt
                     if function_result:
-                        system_prompt = self.valves.TEMPLATE.replace(
+                            system_prompt = self.valves.template.replace(
                             "{{CONTEXT}}", function_result
                         )
-
                         print(system_prompt)
                         messages = add_or_update_system_message(
                             system_prompt, body["messages"]
                         )
-
-                        # Return the updated messages
                         return {**body, "messages": messages}
-
         except Exception as e:
             print(f"Error: {e}")
 
-            if r:
-                try:
-                    print(r.json())
+            try:
+                print(response.json())
                 except:
                     pass
 
